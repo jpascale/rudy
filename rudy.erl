94d9743595cfd52850fe1fbc0b797bfdef2701b2
Juan Pascale <juan.pascale@sabf.org.ar>
2016-09-10 21:23:26 +0200

    Server working

--- rudy.erl
+++ rudy.erl
@@ -3,42 +3,62 @@
 % Server module
 
 -module(rudy).
--export([init/1, handler/1, request/1, reply/1]).
+-export([init/1, handler/1, request/1, reply/1, start/1, stop/0]).
 
 init(Port) ->
 	Opt = [list, {active, false}, {reuseaddr, true}],
 	case gen_tcp:listen(Port, Opt) of
 		{ok, Listen} ->
-
+			handler(Listen),
 			gen_tcp:close(Listen),
 			ok;
 		{error, Error} ->
+			io:format("rudy: error: ~w~n", [Error]),
 			error
 	end.
 
 
 handler(Listen) ->
 	case gen_tcp:accept(Listen) of
-		{ok, Client} ->
-			:
-		{error, Error} ->
-		error
-	end.
+		{ok, Socket} ->
+			request(Socket);
 
-request(Client) ->
-	Recv = gen_tcp:recv(Client, 0),
+		{error, Error} ->
+			io:format("rudy: error: ~w~n", [Error]),
+			error
+	end,
+	handler(Listen).
+
+
+request(Socket) ->
+	Recv = gen_tcp:recv(Socket, 0),
+
 	case Recv of
-		
 		{ok, Str} ->
-			:
+			Request = http:parse_request(Str),
 			Response = reply(Request),
-			gen_tcp:send(Client, Response);
+			gen_tcp:send(Socket, Response);
 
 		{error, Error} ->
 			io:format("rudy: error: ~w~n", [Error])
 	end,
 
-	gen_tcp:close(Client).
+	gen_tcp:close(Socket).
 
 reply({{get, URI, _}, _, _}) ->
-	http:ok(...).
+	http:ok("Requested path is " ++ URI).
+
+
+% RUNSERVER
+
+start(Port) ->
+	register(rudy, spawn(fun() -> init(Port) end)).
+
+stop() ->
+	exit(whereis(rudy), "time to die").
+
+
+
+
+
+
